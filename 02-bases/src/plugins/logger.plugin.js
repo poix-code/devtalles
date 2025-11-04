606678a2b6a1047c99da5804aa021df6a22aecb7
poix-code <proveedor632@culqi.com>
2025-11-04 14:20:04 -0500

    Se mejora estructura de logs

--- 02-bases/src/plugins/logger.plugin.js
+++ 02-bases/src/plugins/logger.plugin.js
@@ -1,8 +1,12 @@
 const winston = require('winston');
+const { combine, timestamp, json } = winston.format;
 
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(
+    timestamp(),
+    json()
+  ),
   //defaultMeta: { service: 'user-service' },
   transports: [
     //
@@ -28,7 +32,10 @@ module.exports = function buildLogger(service) {
             logger.log('info', { message, service});
         },
         error: (message) => {
-            logger.log('error', { message, service });
+            logger.error('error', {
+                message,
+                service
+            });
         }
     };
 }
